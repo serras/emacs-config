6a69fa57bcd90bc8b79d68bd8adfc129ed8b00db
Alejandro Serrano <trupill@gmail.com>
2014-01-14 14:00:32 +0100

    Add Proof General to Emacs

--- dot-emacs.el
+++ dot-emacs.el
@@ -113,13 +113,16 @@
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
+; Coq
+(load-file "~/emacs-config/packages/ProofGeneral-4.2/generic/proof-site.el")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ecb-options-version "2.40")
- '(ecb-source-path (quote (("/home/alejandro/Top/trunk" "Top") ("/home/alejandro/code/sci.f100183.domsted/solver" "solver") ("/home/alejandro/code/sci.f100183.domsted/familiar-type-graphs" "familiar-type-graphs")))))
+ '(ecb-source-path (quote (("/home/alejandro/Top/trunk" "Top") ("/home/alejandro/code/sci.f100183.domsted/solver" "solver")))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
